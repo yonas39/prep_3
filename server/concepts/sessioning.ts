e31cc38b78a3b0b9b35dae6ac73424883de28e4f
Yonas Mekonnen <yonasmek@mit.edu>
2024-09-19 00:35:07 -0400

    bug fixed

--- server/concepts/sessioning.ts
+++ server/concepts/sessioning.ts
@@ -24,7 +24,11 @@ export default class SessioningConcept {
     // Hint: Take a look at how the "end" function makes sure the user is logged in. Keep in mind that a
     // synchronization like starting a session should just consist of a series of actions that may throw
     // exceptions and should not have its own control flow.
-    session.user = username;
+    if (session.user !== undefined) {
+      throw new Error("Log-out before you try to log-in!");
+    } else {
+      session.user = username;
+    }
   }
 
   end(session: SessionDoc) {
